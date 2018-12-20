4d3d2a662fdf13c7f672f89d48a13f308fb65e14
alstjr1419 <alstjr1419@naver.com>
2018-12-20 16:13:42 +0900

    second push

---
+++ exam/exam/js/master.js
@@ -0,0 +1,13 @@
+// var changeSearch = {
+//   flag:true,
+//   styleHandler:function(target){
+//       var txt =  target.children[0];
+//       if(this.flag){
+//         txt.style.width = '240px';
+//         this.flag = false;
+//       }else if(!(this.flag)){
+//         txt.style.width = '0px';
+//         this.flag = true;
+//       }
+//   }
+// };
