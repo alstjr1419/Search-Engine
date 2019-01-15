8e9635b75b6f6532925e62a1983f89029ee0faeb
alstjr1419 <alstjr1419@naver.com>
2019-01-15 17:35:41 +0900

    이벤트 연결중

--- exam/exam/js/master.js
+++ exam/exam/js/master.js
@@ -11,3 +11,30 @@
 //       }
 //   }
 // };
+$(document).ready(function(){
+// searchTxt focus event
+  $('.searchTxt').focus(function(){
+    $(this).val('');
+  });
+  $('.searchTxt').focusout(function(){
+    $(this).val('검색어를 입력하세요.');
+  });
+
+// search icon change handler
+// 미구현..
+  function displayAll(){
+    $('.nonDisplay').removeClass('nonDisplay').addClass('white');
+  }
+  function choiceEngine(){
+    $('.white').removeClass('white').addClass('nonDisplay');
+    $(this).addClass('white').removeClass('nonDisplay');
+  }
+  if($('.white').length==1){
+    $('#choiceEngine').find('.white').click(displayAll());
+  }
+  if($('white').length==3){
+    $('#choiceEngine').find('.white').click(choiceEngine());
+  }
+
+
+});
